300925fdb2a3f9a46366a0d388d4219b1143584a
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-05-02 18:13:26 +0300

    Package of PlayerStats is changed

--- src/main/kotlin/com/zorbeytorunoglu/tdm/game/Game.kt
+++ src/main/kotlin/com/zorbeytorunoglu/tdm/game/Game.kt
@@ -1,8 +1,11 @@
 package com.zorbeytorunoglu.tdm.game
 
+import com.zorbeytorunoglu.tdm.game.player.PlayerStats
+
 class Game(val gameMap: GameMap) {
 
     var redTeam = HashMap<String, PlayerStats>()
     var blueTeam = HashMap<String, PlayerStats>()
+
 
 }
