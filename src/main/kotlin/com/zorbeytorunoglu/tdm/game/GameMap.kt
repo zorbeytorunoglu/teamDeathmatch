601e49bd4be5a3ef3eda999b281605409b9df869
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-05-02 18:14:33 +0300

    Applied new GamePlayer logic.

--- src/main/kotlin/com/zorbeytorunoglu/tdm/game/GameMap.kt
+++ src/main/kotlin/com/zorbeytorunoglu/tdm/game/GameMap.kt
@@ -2,16 +2,31 @@ package com.zorbeytorunoglu.tdm.game
 
 import com.zorbeytorunoglu.tdm.arena.Arena
 import com.zorbeytorunoglu.tdm.arena.ArenaStatus
+import com.zorbeytorunoglu.tdm.game.player.GamePlayer
 import org.bukkit.entity.Player
 
 class GameMap(val arena: Arena) {
 
-    // lobi olsun bu
-
     var status: ArenaStatus = ArenaStatus.WAITING
-    var players: ArrayList<Player> = ArrayList()
-    var redTeam: HashMap<Player, PlayerStats> = HashMap()
-    var blueTeam: HashMap<Player, PlayerStats> = HashMap()
-    var time: Int = arena.time*60
+    val lobbyPlayers: ArrayList<String> = ArrayList()
+    val inGamePlayers = ArrayList<GamePlayer>()
+    val spectators = ArrayList<String>()
+    val time: Int = arena.time*60
+
+    fun playerInGame(player: Player): Boolean {
+
+        if (inGamePlayers.isEmpty()) return false
+
+        return inGamePlayers.any { it.uuid == player.uniqueId.toString() }
+
+    }
+
+    fun playerIsSpectator(player: Player): Boolean {
+
+        if (spectators.isEmpty()) return false
+
+        return spectators.contains(player.uniqueId.toString())
+
+    }
 
 }
