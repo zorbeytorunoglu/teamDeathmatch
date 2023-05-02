74d70b8a92b03d7500c8591ee0983b8e323c4fc6
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-05-02 18:12:29 +0300

    Added CacheManager

---
+++ src/main/kotlin/com/zorbeytorunoglu/tdm/cache/CacheManager.kt
@@ -0,0 +1,53 @@
+package com.zorbeytorunoglu.tdm.cache
+
+import com.zorbeytorunoglu.kLib.configuration.Resource
+import com.zorbeytorunoglu.kLib.extensions.info
+import com.zorbeytorunoglu.kLib.extensions.registerEvents
+import com.zorbeytorunoglu.tdm.TDM
+import com.zorbeytorunoglu.tdm.cache.player.PlayerUUIDCache
+import java.util.*
+
+
+class CacheManager(private val plugin: TDM, private val resource: Resource) {
+
+    init {
+        plugin.registerEvents(PlayerUUIDCache(plugin))
+    }
+
+    val playerUUIDs = HashMap<String, String>()
+
+    fun saveUUIDs() {
+
+        if (playerUUIDs.isEmpty()) return
+
+        for (key in playerUUIDs.keys)
+            resource.set(key,playerUUIDs[key])
+
+        resource.save()
+
+    }
+
+    fun loadUUIDs() {
+
+        val keys = resource.getKeys(false)
+
+        for (key in keys) {
+            try {
+                if (UUID.fromString(resource.getString(key)) != null) {
+                    playerUUIDs[key] = resource.getString(key)!!
+                    plugin.info("$key is cached.")
+                }
+            } catch (ex: Exception) {
+                continue
+            }
+        }
+
+    }
+
+    fun getPlayerName(uuid: UUID): String {
+
+        return playerUUIDs[uuid.toString()]!!
+
+    }
+
+}
