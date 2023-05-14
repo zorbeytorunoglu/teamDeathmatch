4adc30b119eea22e6252d4b6c8c6ef1818f86f73
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-05-15 02:51:26 +0300

    Added more reachable data on game map.

--- src/main/kotlin/com/zorbeytorunoglu/tdm/game/GameMap.kt
+++ src/main/kotlin/com/zorbeytorunoglu/tdm/game/GameMap.kt
@@ -3,6 +3,8 @@ package com.zorbeytorunoglu.tdm.game
 import com.zorbeytorunoglu.tdm.arena.Arena
 import com.zorbeytorunoglu.tdm.arena.ArenaStatus
 import com.zorbeytorunoglu.tdm.game.player.GamePlayer
+import com.zorbeytorunoglu.tdm.game.player.Team
+import com.zorbeytorunoglu.tdm.scoreboard.FastBoard
 import org.bukkit.entity.Player
 
 class GameMap(val arena: Arena) {
@@ -12,6 +14,12 @@ class GameMap(val arena: Arena) {
     val inGamePlayers = ArrayList<GamePlayer>()
     val spectators = ArrayList<String>()
     val time: Int = arena.time*60
+    val gameBoards = HashMap<String, FastBoard>()
+    val lobbyBoards = HashMap<String, FastBoard>()
+
+    fun getBlueTeam(): List<GamePlayer> = inGamePlayers.filter { it.team == Team.BLUE }
+
+    fun getRedTeam(): List<GamePlayer> = inGamePlayers.filter { it.team == Team.RED }
 
     fun playerInGame(player: Player): Boolean {
 
@@ -26,6 +34,19 @@ class GameMap(val arena: Arena) {
         if (spectators.isEmpty()) return false
 
         return spectators.contains(player.uniqueId.toString())
+
+    }
+
+    fun hasMinPlayers(): Boolean = arena.minPlayers <= lobbyPlayers.size
+
+    fun hasSpectator(): Boolean = spectators.isNotEmpty()
+
+    fun getPlayerTeam(player: Player): Team {
+
+        return if (getBlueTeam().any {
+                it.uuid == player.uniqueId.toString()
+            }) Team.BLUE
+        else Team.RED
 
     }
 
